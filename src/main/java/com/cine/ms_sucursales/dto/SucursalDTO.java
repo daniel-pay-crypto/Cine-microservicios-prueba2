bf8927a25ee614892b9d385d00bdf922352e3e55
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-06 17:27:49 -0400

    Refactor: mudanza de archivos a la carpeta independiente ms_sucursales

---
+++ src/main/java/com/cine/ms_sucursales/dto/SucursalDTO.java
@@ -0,0 +1,11 @@
+package Com.cine.ms_sucursales.DTO;
+
+import lombok.Data;
+
+@Data
+public class SucursalDTO {
+    private Long id;
+    private String nombre;
+    private String direccion;
+
+}
