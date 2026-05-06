c326d5f9dbe7cf0e9d6e2c7abc7dfb931aa17385
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-06 19:41:41 -0400

    Created ComunaController

---
+++ src/main/java/com/cine/ms_ubicacion/controller/ComunaController.java
@@ -0,0 +1,32 @@
+package com.cine.ms_ubicacion.controller;
+
+import com.cine.ms_ubicacion.dto.ComunaDTO;
+import com.cine.ms_ubicacion.service.ComunaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/comunas")
+@RequiredArgsConstructor
+public class ComunaController {
+
+    private final ComunaService comunaService;
+
+    @GetMapping
+    public ResponseEntity<List<ComunaDTO>> listarTodas() {
+        return ResponseEntity.ok(comunaService.listarTodas());
+    }
+
+    @PostMapping("/region/{regionId}")
+    public ResponseEntity<ComunaDTO> guardar(
+            @Valid @RequestBody ComunaDTO comunaDTO,
+            @PathVariable Long regionId) {
+        
+        ComunaDTO nuevaComuna = comunaService.guardar(comunaDTO, regionId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaComuna);
+    }
+}
