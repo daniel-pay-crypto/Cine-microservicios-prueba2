e928e96be82998122ab8b029b78a2e9986f6c5e3
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-08 17:48:47 -0400

    Update ubicacionApplication

--- src/main/java/com/cine/ms_ubicacion/MsUbicacionApplication.java
+++ src/main/java/com/cine/ms_ubicacion/MsUbicacionApplication.java
@@ -3,10 +3,12 @@ package com.cine.ms_ubicacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cine.ms_salas.MsSalaApplication;
+
 @SpringBootApplication
 public class MsUbicacionApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MsUbicacionApplication.class, args);
+    public static void main(String[] args){
+        System.setProperty("spring.profiles.active", "ubicacion");
+        SpringApplication.run(MsSalaApplication.class, args);
     }
 }
