6c6faa7174f1242b9c40f572fd25c886221edd8c
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-08 17:47:50 -0400

    Update SucursalesApplication

--- src/main/java/com/cine/ms_sucursales/MsSucursalesApplication.java
+++ src/main/java/com/cine/ms_sucursales/MsSucursalesApplication.java
@@ -3,11 +3,14 @@ package com.cine.ms_sucursales;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cine.ms_salas.MsSalaApplication;
+
 @SpringBootApplication
 public class MsSucursalesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsSucursalesApplication.class, args);
-	}
+	public static void main(String[] args){
+        System.setProperty("spring.profiles.active", "sucursales");
+        SpringApplication.run(MsSalaApplication.class, args);
+    }
 
 }
