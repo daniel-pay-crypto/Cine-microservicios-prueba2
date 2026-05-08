785618d0182387c4867fdb699ffd424409ea9d4e
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-08 17:46:09 -0400

    Update

--- src/main/java/com/cine/ms_salas/MsSalaApplication.java
+++ src/main/java/com/cine/ms_salas/MsSalaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsSalaApplication { 
     public static void main(String[] args){
-        System.setProperty("spring.porfiles.active", "salas");
+        System.setProperty("spring.profiles.active", "salas");
         SpringApplication.run(MsSalaApplication.class, args);
         }
     
