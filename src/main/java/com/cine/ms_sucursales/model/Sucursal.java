30b85e3eee1034a1e70f8270d3b8b6858c837ae7
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-08 17:58:51 -0400

    Update Sucursal

--- src/main/java/com/cine/ms_sucursales/model/Sucursal.java
+++ src/main/java/com/cine/ms_sucursales/model/Sucursal.java
@@ -1,11 +1,13 @@
 package com.cine.ms_sucursales.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,4 +30,8 @@ public class Sucursal {
 
     @NotBlank(message = "Debes ingresar una direccion valida")
     private String direccion;
+
+    @NotNull(message = "El ID de la comuna es obligatorio")
+    @Column(name = "comuna_id")
+    private Long comunaId;
 }
