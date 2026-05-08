ccc552249fa7f9fd5fe443bddac9b28e5fbdb355
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-07 21:11:21 -0400

    Create MsSalaApplication

---
+++ src/main/java/com/cine/ms_salas/MsSalaApplication.java
@@ -0,0 +1,12 @@
+package com.cine.ms_salas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSalaApplication.class, args);
+    }
+}
