2f176b5008ad71370dea39269bfc4bf241a821d0
daniel <da.leytonc@duocuc.cl>
2026-05-09 17:12:03 -0400

    Commit de Configuración de puerto 8082 yla conexión autogenerada a BD db_cine_pelicula

--- ms-pelicula/ms-pelicula/src/main/java/com/cine/ms_pelicula/MsPeliculaApplication.java
+++ ms-pelicula/ms-pelicula/src/main/java/com/cine/ms_pelicula/MsPeliculaApplication.java
@@ -2,8 +2,10 @@ package com.cine.ms_pelicula;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@FeignClient // Configuración de Feign Client para comunicarse con ms-cliente
 public class MsPeliculaApplication {
 
 	public static void main(String[] args) {
