52b2320d8c3fc4b928b1c99a3ac956e9dc1708cb
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-08 20:36:34 -0400

    Create SalasDTO

---
+++ src/main/java/com/cine/ms_salas/dto/SalasDTO.java
@@ -0,0 +1,19 @@
+package com.cine.ms_salas.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class SalasDTO {
+    private Long id;
+
+    @NotNull(message = "El ID de la sala física es obligatorio")
+    private Long salaId;
+
+    @NotNull(message = "El ID de la película es obligatorio")
+    private Long peliculaId;
+
+    private LocalDateTime fechaInicio;
+}
