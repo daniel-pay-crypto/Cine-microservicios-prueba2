eec6c81119bf5c7e897e258251a9a6d1568a88d8
dlee-ux <dael.escobedo.c@gmail.com>
2026-05-09 01:12:38 -0400

    MSgeneroApplication
    implementar GeneroRepository con Spring Data JPA

---
+++ src/main/java/com/cine/ms_genero/MsGeneroApplication.java
@@ -0,0 +1,13 @@
+package com.cine.ms_genero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGeneroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGeneroApplication.class, args);
+	}
+
+}
