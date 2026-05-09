76df14665a4a916b01a69f8c066473e9cdf5fbe7
daniel <da.leytonc@duocuc.cl>
2026-05-09 16:40:59 -0400

    Implementacion de la carpeta para el microservicio de peliculas :D

---
+++ ms-pelicula/ms-pelicula/src/main/java/com/cine/ms_pelicula/MsPeliculaApplication.java
@@ -0,0 +1,13 @@
+package com.cine.ms_pelicula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPeliculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPeliculaApplication.class, args);
+	}
+
+}
