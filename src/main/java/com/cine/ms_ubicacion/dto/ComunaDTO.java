8b93a39f3c94f8e8c415304fe71757fd87ecc6aa
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-09 22:10:41 -0400

    Update ComunaDTO

--- src/main/java/com/cine/ms_ubicacion/dto/ComunaDTO.java
+++ src/main/java/com/cine/ms_ubicacion/dto/ComunaDTO.java
@@ -1,5 +1,7 @@
 package com.cine.ms_ubicacion.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ComunaDTO {
-
+    
+    @NotNull(message = "El Id de la comuna no puede ir vacia")
     private Long id;
+
+    @NotBlank(message = "El nombre de la comuna no puede ir vacia")
     private String nombre;
+
+    @NotBlank(message = "El nombre de la region no puede ir vacia")
     private String nombreRegion; //Este lo agrege para q sea mas facil de leer el el front
 
 }
