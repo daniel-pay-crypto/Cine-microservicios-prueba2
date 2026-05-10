4aaf2f40eb9ff48a267ee3e660d096d41149a30b
daniel <da.leytonc@duocuc.cl>
2026-05-10 12:32:05 -0400

    cambios en el model y solucion a errores con los mapas de conexion con los demas microservicios

--- ms-pelicula/ms-pelicula/src/main/java/com/cine/ms_pelicula/model/Pelicula.java
+++ ms-pelicula/ms-pelicula/src/main/java/com/cine/ms_pelicula/model/Pelicula.java
@@ -1,27 +1,39 @@
 package com.cine.ms_pelicula.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "peliculas")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pelicula {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String titulo;
-    private String sinopsis;
+
+    @Column(name = "duracion_minutos")
     private Integer duracionMinutos;
 
-    // Relaciones 1:N con ms-genero y ms-director.
+   
+    @Column(name = "genero_id", nullable = false)
     private Long generoId;
+
+    @Column(name = "director_id", nullable = false)
     private Long directorId;
+
 }
