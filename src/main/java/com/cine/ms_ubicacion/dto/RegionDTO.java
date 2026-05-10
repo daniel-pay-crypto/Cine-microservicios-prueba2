ed6dd1f940300285f5f37ed76e361937f19e4697
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-09 22:12:21 -0400

    Update RegionDTO

--- src/main/java/com/cine/ms_ubicacion/dto/RegionDTO.java
+++ src/main/java/com/cine/ms_ubicacion/dto/RegionDTO.java
@@ -1,5 +1,7 @@
 package com.cine.ms_ubicacion.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegionDTO {
+
+    @NotNull(message = "El ID de la region no puede estar vacia")
     private Long id;
+
+    @NotBlank(message = "El nombre de la region no puede estar vacia")
     private String nombre;
 }
