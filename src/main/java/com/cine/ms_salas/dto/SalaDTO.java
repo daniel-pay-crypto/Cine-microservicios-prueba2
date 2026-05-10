d074a6b34dc595a34b0cf010d915fa9638890e29
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-09 21:57:23 -0400

    Update SalaDTO

--- src/main/java/com/cine/ms_salas/dto/SalaDTO.java
+++ src/main/java/com/cine/ms_salas/dto/SalaDTO.java
@@ -1,5 +1,8 @@
 package com.cine.ms_salas.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SalaDTO {
+
+    @NotNull(message = "El Id de la sala es obligatorio")
     private Long id;
+    
+    @NotBlank(message = "El nombre de la sala no puede estar vacio")
     private String nombre;
+
+    @NotNull(message = "La capacidad de la sala es obligatorio")
+    @Min(value = 10, message = "El valor minimo de capacidad es 10")
     private Integer capacidad;
+
+    @NotNull(message = "El ID es obligatorio")
     private Long sucursalId;
 }
