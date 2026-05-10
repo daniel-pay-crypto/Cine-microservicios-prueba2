d8c3d229852a45e1f55ca609149abe020ebd6703
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-09 22:49:47 -0400

    Clean code

--- src/main/java/com/cine/ms_salas_plural/repository/SalasRepository.java
+++ src/main/java/com/cine/ms_salas_plural/repository/SalasRepository.java
@@ -2,7 +2,6 @@ package com.cine.ms_salas_plural.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.cine.ms_salas_plural.model.Salas;
 
 @Repository
