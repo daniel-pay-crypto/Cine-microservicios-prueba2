7d33bcf22e9d5e2975cc21b664a195321e9aa9b6
daniel <da.leytonc@duocuc.cl>
2026-05-09 20:29:45 -0400

    Implementacion de microservicio de Director (tuve un pequeño error de carpetas al igual que los otros proyectos, lamentola falta de profesionalismo)

---
+++ ms-director/src/main/java/com/cine/ms_director/MsDirectorApplication.java
@@ -0,0 +1,13 @@
+package com.cine.ms_director;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDirectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDirectorApplication.class, args);
+	}
+
+}
---
+++ ms-director/src/main/java/com/cine/ms_director/model/Director.java
@@ -0,0 +1,21 @@
+package com.cine.ms_director.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "directores")
+@Data
+public class Director {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombres;
+    private String apellidos;
+    private String nacionalidad;
+}
