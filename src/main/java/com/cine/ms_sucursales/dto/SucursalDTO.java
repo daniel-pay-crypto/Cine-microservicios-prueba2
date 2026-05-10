3aae93bcb3afbc07c90fb1fc8a6ab1ed956d826e
Thomás Osorio <th.osorio@duocuc.cl>
2026-05-09 22:04:47 -0400

    Update SucursalDTO

--- src/main/java/com/cine/ms_sucursales/dto/SucursalDTO.java
+++ src/main/java/com/cine/ms_sucursales/dto/SucursalDTO.java
@@ -1,5 +1,8 @@
 package com.cine.ms_sucursales.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +13,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SucursalDTO {
+
+    @NotNull(message = "El Id no puede estar vacio")
     private Long id;
+
+    @NotBlank(message = "El nombre de la sucursal no debe estar vacio")
     private String nombre;
+
+    @NotBlank(message = "La direccion no puede estar vacia")
     private String direccion;
+
+    @NotNull(message = "El Id de la comuna no puede ir vacia")
     private Long comunaid;
 }
