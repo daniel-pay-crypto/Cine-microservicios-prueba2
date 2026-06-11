ba6d71f5f9acbd01090f2276fc366a826e337b23
Thomys229 <th.osorio@duocuc.cl>
2026-06-10 22:11:50 -0400

    Update Exception

--- ms_salas/src/main/java/com/cine/ms_salas/exception/GlobalExceptionHandler.java
+++ ms_salas/src/main/java/com/cine/ms_salas/exception/GlobalExceptionHandler.java
@@ -1,32 +1,39 @@
 package com.cine.ms_salas.exception;
 
-
+import com.cine.ms_salas.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import java.time.LocalDateTime;
 
-@ControllerAdvice
+import org.springframework.web.ErrorResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    //Esto atrapa los errores de las validaciones (@NotNull, @NotBlank)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> manejarValidaciones(MethodArgumentNotValidException ex) {
-        Map<String, String> errores = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> 
-            errores.put(error.getField(), error.getDefaultMessage())
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(), // 404
+                "Not Found",
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=", "")
         );
-        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
-    //Esto atrapa cualquier otro error inesperado
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Map<String, String>> manejarErroresGenerales(RuntimeException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", ex.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(), // 500
+                "Error Interno del Servidor",
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=", "")
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
