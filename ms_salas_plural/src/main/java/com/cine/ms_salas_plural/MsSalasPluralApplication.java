5d123e8a4a283b3fa057f93192b132ec17debdcd
Thomys229 <th.osorio@duocuc.cl>
2026-06-18 18:24:19 -0400

    Update MsSalasPluralApplication

--- ms_salas_plural/src/main/java/com/cine/ms_salas_plural/MsSalasPluralApplication.java
+++ ms_salas_plural/src/main/java/com/cine/ms_salas_plural/MsSalasPluralApplication.java
@@ -2,10 +2,8 @@ package com.cine.ms_salas_plural;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class MsSalasPluralApplication {
 
     public static void main(String[] args) {
