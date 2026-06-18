0e2cd7725c643b561972eee90249c36bdfc38f1d
Thomys229 <th.osorio@duocuc.cl>
2026-06-18 17:20:15 -0400

    Update MsSalasApplication

--- ms_salas/src/main/java/com/cine/ms_salas/MsSalasApplication.java
+++ ms_salas/src/main/java/com/cine/ms_salas/MsSalasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients 
+@EnableFeignClients
 public class MsSalasApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsSalasApplication.class, args);
