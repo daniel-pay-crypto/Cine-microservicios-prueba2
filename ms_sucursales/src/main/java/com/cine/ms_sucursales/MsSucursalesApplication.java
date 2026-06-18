6ebae269e3312750f159e8e517f0ed344dc87008
Thomys229 <th.osorio@duocuc.cl>
2026-06-18 18:14:24 -0400

    Update MsSucursalesApplication

--- ms_sucursales/src/main/java/com/cine/ms_sucursales/MsSucursalesApplication.java
+++ ms_sucursales/src/main/java/com/cine/ms_sucursales/MsSucursalesApplication.java
@@ -2,10 +2,9 @@ package com.cine.ms_sucursales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableFeignClients 
+
+@SpringBootApplication 
 public class MsSucursalesApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsSucursalesApplication.class, args);
