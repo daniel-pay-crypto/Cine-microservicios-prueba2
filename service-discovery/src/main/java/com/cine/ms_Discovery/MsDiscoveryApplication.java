f1fba7c3fc3028e444c34557644d4e34e533191b
daniel <da.leytonc@duocuc.cl>
2026-06-21 22:50:21 -0400

    commit: Creo service discovery para las pruebas de integracion .

---
+++ service-discovery/src/main/java/com/cine/ms_Discovery/MsDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.cine.ms_Discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDiscoveryApplication.class, args);
+	}
+
+}
