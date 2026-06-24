f99eb049c97bdc848a4b14090eeb7e161e592147
Thomys229 <th.osorio@duocuc.cl>
2026-06-24 16:01:05 -0400

    Update WebClient

--- ms_ubicacion/src/main/java/com/cine/ms_ubicacion/config/WebClientConfig.java
+++ ms_ubicacion/src/main/java/com/cine/ms_ubicacion/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.cine.ms_ubicacion.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,9 +9,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder()
-                .baseUrl("http://localhost:8082") 
-                .build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
